ce13cc244d34733ff644db7593d4217c43948c7f
Yilan He <Yilan He>
2016-02-03 20:36:17 +0800

    init

---
+++ app/src/main/java/com/example/android/ricepouch/TabFragment1.java
@@ -0,0 +1,16 @@
+package com.example.android.ricepouch;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Yilan on 2016/1/31.
+ */
+public class TabFragment1 extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+    }
+}
